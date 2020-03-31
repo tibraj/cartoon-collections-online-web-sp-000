55ef9b42f3d71fd09515a450a0864b909ff85f3a
Taulant Ibraj <tibraj94@gmail.com>
2020-03-31 15:27:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types[0])
-  array.include?(cheese_types[1])
-  array.include?(cheese_types[2])
+  array.detect do |i|
+    if i === cheese_types[0] || i === cheese_types[1] || i === cheese_types[2]
+      return array[i]
 end
