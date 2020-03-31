eaa9667d0c560f67e873991be28f58187ec1b457
Taulant Ibraj <tibraj94@gmail.com>
2020-03-31 14:51:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |element|
+    element.capitalize
+  end
 end
 
 def long_planeteer_calls# code an argument here
