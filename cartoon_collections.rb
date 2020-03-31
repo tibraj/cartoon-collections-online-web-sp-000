8052af492eded0456e8c5bab67b9837441328caa
Taulant Ibraj <tibraj94@gmail.com>
2020-03-31 15:23:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls(array)
   array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types[0])
+  array.include?(cheese_types[1])
+  array.include?(cheese_types[2])
 end
